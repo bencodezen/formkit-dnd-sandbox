f5cd26661b7840576bf01b7bb01030593704557d
Ben Hong <ben@bencodezen.io>
2024-02-28 12:42:38 -0800

    feat: add drag between two lists

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useDragAndDrop } from '@formkit/drag-and-drop/vue'
 
-const [parent, crew] = useDragAndDrop(['Luffy', 'Zoro', 'Nami', 'Sanji', 'Usopp'])
+const crew1 = ref(['Luffy', 'Zoro', 'Sanji', 'Nami', 'Usopp'])
+const crew2 = ref(['Chopper', 'Franky', 'Robin', 'Brook', '???'])
+
+const [elParent, renderedCrew] = useDragAndDrop(crew1.value)
+const [elCrew1, renderedCrew1] = useDragAndDrop(crew1.value, { group: 'crew' })
+const [elCrew2, renderedCrew2] = useDragAndDrop(crew2.value, { group: 'crew' })
 </script>
 
 <template>
   <h1>FormKit DnD Experiments</h1>
   <h2>Sorting a List with Drag and Drop</h2>
-  <ol ref="parent">
-    <li v-for="member in crew" :key="member">{{ member }}</li>
+  <ol ref="elParent">
+    <li v-for="member in renderedCrew" :key="member">{{ member }}</li>
   </ol>
+  <h2>Dragging Between Two Lists</h2>
+  <div style="display: grid; grid-template-columns: 1fr 1fr">
+    <ul ref="elCrew1">
+      <li v-for="member in renderedCrew1" :key="member">{{ member }}</li>
+    </ul>
+    <ul ref="elCrew2">
+      <li v-for="member in renderedCrew2" :key="member">{{ member }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
